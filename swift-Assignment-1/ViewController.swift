4ef5f02f36020ae896e51fbcd90c208df5486c58
aslancll <celalaslan@outlook.com>
2018-05-31 14:42:46 -0700

    Initial Commit

---
+++ swift-Assignment-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-Assignment-1
+//
+//  Created by Celal Aslan on 2018-05-31.
+//  Copyright © 2018 Celal Aslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
